afa5bf8c8193af7ebe5b0aac9a6e2799943f8255
Primemaker <infamywong@gmail.com>
2020-11-24 15:37:38 +0800

    initial commit

---
+++ process_graph.py
@@ -0,0 +1,12 @@
+#encoding = utf-8
+import os
+
+if __name__ == "__main__":
+    data_name = "dynamic_tsne_final"
+    data_path = os.path.join(
+        "/Users/joe/Codes/PythonProjects/joint_tsne_experiments/data",
+        data_name)
+
+    graph_path = os.path.join(
+        "/Users/joe/Codes/PythonProjects/joint_tsne_experiments/graph",
+        data_name)
